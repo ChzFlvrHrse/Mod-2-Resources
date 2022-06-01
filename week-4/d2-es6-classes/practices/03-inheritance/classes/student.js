ee10bd0bdf4588a850d150176b959d176c4fdaa4
nate <nscott1010@gmail.com>
2022-06-01 11:47:26 -0500

    inheritance

--- week-4/d2-es6-classes/practices/03-inheritance/classes/student.js
+++ week-4/d2-es6-classes/practices/03-inheritance/classes/student.js
@@ -1,6 +1,26 @@
 const Person = require('./person');
 
-// Your code here
+class Student extends Person{
+  constructor(firstName, lastName, major, GPA) {
+    super(firstName, lastName)
+    this.major = major;
+    this.GPA = GPA;
+  }
+
+  introduce() {
+    return `Hello. I am ${this.firstName} ${this.lastName}, and I study ${this.major}.`
+  }
+
+  static compareGPA(student1, student2) {
+    if (student1.GPA > student2.GPA) {
+      return `${student1.firstName} ${student1.lastName} has the higher GPA.`;
+    } else if (student1.GPA < student2.GPA) {
+      return `${student2.firstName} ${student2.lastName} has the higher GPA.`;
+    } else {
+      return "Both students have the same GPA.";
+    }
+  }
+}
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
--- week-4/d2-es6-classes/practices/03-inheritance/classes/teacher.js
+++ week-4/d2-es6-classes/practices/03-inheritance/classes/teacher.js
@@ -1,6 +1,22 @@
 const Person = require('./person');
 
-// Your code here
+class Teacher extends Person {
+  constructor(firstName, lastName, subject, yearsOfExperience) {
+    super(firstName, lastName);
+    this.subject = subject;
+    this.yearsOfExperience = yearsOfExperience;
+  }
+
+  introduce() {
+    return `Hello. I am ${this.firstName} ${this.lastName}, and I have been teaching ${this.subject} for ${this.yearsOfExperience} years.`
+  }
+
+  static combinedYearsOfExperience(array) {
+    return array.reduce(function(acc, el) {
+      return acc.yearsOfExperience + el.yearsOfExperience;
+    })
+  }
+}
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
