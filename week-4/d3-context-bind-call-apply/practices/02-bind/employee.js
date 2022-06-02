a1a8050f8c6da4b519dc8c6d8720418a0fe79668
nate <nscott1010@gmail.com>
2022-06-02 10:40:45 -0500

    bind

--- week-4/d3-context-bind-call-apply/practices/02-bind/employee.js
+++ week-4/d3-context-bind-call-apply/practices/02-bind/employee.js
@@ -1,1 +1,16 @@
-// Your code here
+class Employee {
+  constructor(name, occupation) {
+    this.name = name;
+    this.occupation = occupation;
+  }
+
+  sayName() {
+    return `${this.name} says hello`
+  }
+
+  sayOccupation() {
+    return `${this.name} is a ${this.occupation}`
+  }
+}
+
+module.exports = Employee;
--- week-4/d3-context-bind-call-apply/practices/02-bind/test.js
+++ week-4/d3-context-bind-call-apply/practices/02-bind/test.js
@@ -1,1 +1,12 @@
-// Your code here
+const Employee = require('./employee')
+
+let john = new Employee("John Wick", "Dog Lover");
+
+// setTimeout(jWick.sayName, 2000);
+// setTimeout(jWick.sayOccupation, 2000);
+
+let boundSayName = jWick.sayName.bind(jWick);
+let boundSayOccupation = jWick.sayOccupation.bind(jWick);
+
+setTimeout(boundSayName, 2000);
+setTimeout(boundSayOccupation, 2000);
