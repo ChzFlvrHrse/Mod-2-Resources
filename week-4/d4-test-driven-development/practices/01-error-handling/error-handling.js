b858b8e37b719b803107182265cb9c04daa6ad0a
nate <nscott1010@gmail.com>
2022-06-03 10:29:20 -0500

    error handling

--- week-4/d4-test-driven-development/practices/01-error-handling/error-handling.js
+++ week-4/d4-test-driven-development/practices/01-error-handling/error-handling.js
@@ -1,26 +1,39 @@
 // 1.
 function sum(array) {
   let sum = 0;
-  for (let i = 0; i < array.length; i++) {
-    sum += array[i];
+  try {
+    for (let i = 0; i < array.length; i++) {
+      sum += array[i];
+    }
+  } catch (e) {
+    throw new Error("Argument must be an array");
   }
   return sum;
 }
 
+console.log(sum([1, 2, 3])) // prints 6
 let res = sum(null);
-console.log(res);
+console.log(res); // throws error
 
 // 2.
 // tests
-sayName("Alex");
-sayName(1);
-// Your code here
+function sayName(name) {
+  if (typeof name === "string") {
+    return name;
+  } else {
+      throw new TypeError("Argument must be string")
+  }
+}
+console.log(sayName("Alex"));
+console.log(sayName(1));
 
 // 3.
 function greet(greeting) {
   if (!greeting) {
     throw new Error("There was no greeting given.");
   }
-
-  console.log(greeting);
+  return greeting;
 }
+
+console.log(greet("Hello there!")) // prints greeting
+console.log(greet()) // throws error
