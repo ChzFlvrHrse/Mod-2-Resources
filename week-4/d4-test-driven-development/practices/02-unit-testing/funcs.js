1d32588bb53a240039d5d174f97553fe165e2150
nate <nscott1010@gmail.com>
2022-06-03 11:12:07 -0500

    function tests

--- week-4/d4-test-driven-development/practices/02-unit-testing/funcs.js
+++ week-4/d4-test-driven-development/practices/02-unit-testing/funcs.js
@@ -1,13 +1,25 @@
 function isFive(num) {
-  // Your code here
+  return num === 5;
 }
 
 function isOdd(number) {
-  // Your code here
+  if (typeof number === 'number') {
+    return number % 2 !== 0;
+  } else {
+    throw new Error("Argument must be a number")
+  }
 }
 
 function myRange(min, max, step = 1) {
-  // Your code here
+  let array = [];
+  if (min < max) {
+    for (let i = 0; i <= max; i+=step) {
+      array.push(min + i)
+    }
+    return array;
+  } else {
+    return [];
+  }
 }
 
 
