f6ecac6ba1d249125da7c79a05096731faf93ad4
nate <nscott1010@gmail.com>
2022-06-06 15:44:36 -0500

    phase-2

--- week-5/d1-big-o-arrays/practices/01-timing-benchmarks/phase-2.js
+++ week-5/d1-big-o-arrays/practices/01-timing-benchmarks/phase-2.js
@@ -2,14 +2,20 @@ const [addNums, addManyNums] = require("./phase-1");
 
 // Runs `addNums` in 10 increasing increments
 function addNums10(increment) {
-  // Fill this in
-
+  let nums = [];
+  for (let i = 1; i <= 10; i++) {
+    nums.push(addNums(i * increment))
+  }
+  return nums
 }
 
 // Runs `addManyNums` in 10 increasing increments
 function addManyNums10(increment) {
-  // Fill this in
-
+  let nums = [];
+  for (let i = 1; i <= 10; i++) {
+    nums.push(addManyNums(i * increment))
+  }
+  return nums;
 }
 
 module.exports = [addNums10, addManyNums10];
