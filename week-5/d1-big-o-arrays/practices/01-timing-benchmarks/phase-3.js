02c3fcc4e5e661c21ead460079dad354633fcecf
nate <nscott1010@gmail.com>
2022-06-06 16:03:30 -0500

    phase 3

--- week-5/d1-big-o-arrays/practices/01-timing-benchmarks/phase-3.js
+++ week-5/d1-big-o-arrays/practices/01-timing-benchmarks/phase-3.js
@@ -1,29 +1,33 @@
 const [addNums, addManyNums] = require("./phase-1");
 
 function addNums10Timing(increment) {
-  // Copy your `addNums10` code here
-  // Then, add timing code
-
-  // Your code here
-
+  let nums = [];
+  for (let i = 1; i <= 10; i++) {
+    nums.push(addNums(i * increment))
+  }
+  return nums
 }
 
 
 function addManyNums10Timing(increment) {
-// Copy your `addManyNums10` code here
-// Then, add timing code
-
-  // Your code here
-
+  let nums = [];
+  for (let i = 1; i <= 10; i++) {
+    nums.push(addManyNums(i * increment))
+  }
+  return nums;
 }
 
 
 n = 1000000
+console.time("addNums")
 console.log(`addNums(${n}): `);
 addNums10Timing(1000000);
+console.timeEnd("addNums")
 
 console.log("\n***********\n");
 
 n = 1000
+console.time("addManyNums")
 console.log(`addManyNums(${n}): `);
 addManyNums10Timing(5000);
+console.timeEnd("addManyNums")
