2d989f277a8a08dc4ac6eebaf7514e1fcb2cdcfd
nate <nscott1010@gmail.com>
2022-06-07 11:17:00 -0500

    dynamic array

--- week-5/d2-binary-hexa-ascii-memory-arrays/practices/03-dynamic-arrays/dynamic-array.js
+++ week-5/d2-binary-hexa-ascii-memory-arrays/practices/03-dynamic-arrays/dynamic-array.js
@@ -1,19 +1,62 @@
 class DynamicArray {
 
-  constructor(defaultSize=4) {
+  constructor(defaultSize = 4) {
+    this.defaultSize = defaultSize;
 
-    // Your code here
-
+    this.capacity = this.defaultSize;
+    this.length = 0;
+    this.data = new Array(this.defaultSize);
   }
 
   read(index) {
+    if (index >= this.length) {
+      return undefined;
+    }
+    return this.data[index];
+  }
 
-    // Your code here
+  push(val) {
+    this.resize()
+    this.data[this.length] = val;
+    this.length++
+  }
+
+
+  pop() {
+    let popped = this.data.splice(this.length - 1, 1);
+    this.length--
+    return popped[0]
+  }
+
+  shift() {
+    let shifted = this.data.splice(0, 1);
+    this.length--;
+    return shifted[0];
   }
 
   unshift(val) {
+    this.resize();
+    for (let i = this.length; i > 0; i--) {
+      this.data[i] = this.data[i - 1]
+    }
+    this.data[0] = val;
+    this.length++
+    // this.resize();
+  }
 
-    // Your code here
+  indexOf(val) {
+    for (let i = 0; i < this.length; i++) {
+      if (val === this.data[i]) {
+        return i
+      }
+    }
+    return -1
+  }
+
+  resize() {
+    if (this.length === this.capacity) {
+      this.capacity *= 2;
+    }
   }
 
 }
