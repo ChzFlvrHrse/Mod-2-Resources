f57b9fa1cc2a0a689b9ab863003cd77b61fac421
nate <nscott1010@gmail.com>
2022-06-08 10:52:06 -0500

    doubly linked list

--- week-5/d3-linked-lists-and-queues/practices/01-linked-lists/doubly-linked-list.js
+++ week-5/d3-linked-lists-and-queues/practices/01-linked-lists/doubly-linked-list.js
@@ -8,15 +8,36 @@ class DoublyLinkedListNode {
 
 class DoublyLinkedList {
   constructor() {
-    // Your code here
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
   }
 
   addToHead(val) {
-    // Your code here
+    let node = new DoublyLinkedListNode(val);
+    if (!this.head) {
+      this.head = node;
+      this.tail = node;
+    } else {
+      node.next = this.head;
+      node.next.prev = node;
+      this.head = node;
+    }
+    this.length++;
   }
 
   addToTail(val) {
-    // Your code here
+    let node = new DoublyLinkedListNode(val);
+
+    if (!this.head) {
+      this.head = node;
+      this.tail = node;
+    } else {
+      node.prev = this.tail;
+      this.tail.next = node;
+      this.tail = node;
+    }
+    this.length++;
   }
 
   // You can use this function to help debug
