1cadd1ef28dc733483a6350a8a3c537a68c47389
nate <nscott1010@gmail.com>
2022-06-13 16:52:18 -0500

    selection sort

--- week-6/d1-sorting-bubble-insertion-selection/practices/03-selection-sort/selection-sort.js
+++ week-6/d1-sorting-bubble-insertion-selection/practices/03-selection-sort/selection-sort.js
@@ -1,38 +1,53 @@
-
-
 function selectionSort(arr) {
-
+  let copy = [...arr];
   // Copy the original array
-
+  let sorted = [];
   // Create an array to store the sorted values
-
-  // While the array is not empty...
-
-    // Do not move this console.log
+  while(copy.length) {
     console.log(sorted.join(","));
 
-    // Find the index of the minimum value in the unsorted half
-
-    // Save and remove the value at the min index
-
-    // Add the min value to the end of the sorted array
-
+    let minimum = copy.reduce(function(acc, el) {
+      if (acc > el) {
+        return el;
+      }
+      return acc;
+    });
+    let minRemove = copy.splice(copy.indexOf(minimum), 1)
+    sorted.push(...minRemove);
+  }
+  return sorted;
 }
 
 
 
 function selectionSortInPlace(arr) {
+  let div = 0;
 
-  // Set a pointer at zero diving the array into sorted and unsorted halves
-
-  // Repeat while the unsorted half is not empty:
-
-    // Do not move this console.log
+  while (div < arr.length) {
     console.log(arr.join(","));
 
     // Find the index of the minimum value in the unsorted half
+    let minIndex = div;
+    for (let i = div + 1; i < arr.length; i++) {
+      if (arr[i] < arr[minIndex]) minIndex = i;
+    }
 
     // Save the min value
+    let minVal = arr[minIndex];
+
+    // Shift every unsorted value to the left of the min value to the right by 1
+    for (let i = minIndex; i >= div; i--) {
+      arr[i] = arr[i - 1];
+    }
+
+    // Put the min value at the div
+    arr[div] = minVal;
+
+    // Increment the div and repeat
+    div++;
+  }
+  return arr;
+}
 
     // Shift every unsorted value to the left of the min value to the right by 1
 
@@ -40,7 +55,6 @@ function selectionSortInPlace(arr) {
 
     // Increment the divider and repeat
 
-}
 
 
 module.exports = [selectionSort, selectionSortInPlace];
