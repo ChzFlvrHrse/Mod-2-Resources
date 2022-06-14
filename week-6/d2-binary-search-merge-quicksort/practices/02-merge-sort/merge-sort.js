4b7eab2c098d41f97b7c3c3cfce7aacb842aa483
nate <nscott1010@gmail.com>
2022-06-14 11:04:59 -0500

    merge sort

--- week-6/d2-binary-search-merge-quicksort/practices/02-merge-sort/merge-sort.js
+++ week-6/d2-binary-search-merge-quicksort/practices/02-merge-sort/merge-sort.js
@@ -2,19 +2,17 @@
 // Do not modify the original array
 function mergeSort(arr) {
   // Check if the input is length 1 or less
-  if (arr.length <= 1) {
-    // If so, it's already sorted: return
-    return arr;
-  }
+
+  if (arr.length <= 1) return arr;
   // Divide the array in half
   let mid = Math.floor(arr.length / 2);
   let left = arr.slice(0, mid);
   let right = arr.slice(mid, arr.length)
 
-  // Recursively sort the left half
-  let leftSorted = mergeSort(left)
-  // Recursively sort the right half
-  let rightSorted = mergeSort(right)
+
+  let leftSorted = mergeSort(left)// Recursively sort the left half
+  let rightSorted = mergeSort(right)// Recursively sort the right half
+
   // Merge the halves together and return
   return merge(leftSorted, rightSorted)
 }
@@ -40,5 +38,31 @@ function merge(arrA, arrB) {
   // Return the return array
   return arr;
 }
+
+// function merge(arrA, arrB) {
+
+//   // Create an empty return array
+//   let arr = [];
+//   // Point to the first value of each array
+//   let indexA = 0;
+//   let indexB = 0;
+
+//   while (indexA < arrA.length || indexB < arrB.length) {
+//     if (indexA === arrA.length) {
+//       arr.push(arrB[indexB]);
+//       indexB++
+//     } else if (indexB === arrB.length) {
+//         arr.push(arrA[indexA]);
+//         indexA++;
+//     } else if (arrA[indexA] < arrB[indexB]) {
+//         arr.push(arrA[indexA]);
+//         indexA++;
+//     } else {
+//         arr.push(arrB[indexB]);
+//         indexB++
+//     }
+//   }
+//   return arr;
+// }
 
 module.exports = [merge, mergeSort];
