efed09f01d41c054a0b6b0f47a2f99122848893f
nate <nscott1010@gmail.com>
2022-06-16 12:26:08 -0500

    refactor bft

--- week-6/d4-graphs/practices/01-graph-repl-problems/morning-practice/03-refactor-bft.js
+++ week-6/d4-graphs/practices/01-graph-repl-problems/morning-practice/03-refactor-bft.js
@@ -8,15 +8,32 @@ const adjList = {
 }
 
 function printBreadthFirst(start) {
-    // Paste your previous code here and refactor it
-}
+    let queue = [start];
+    let set = new Set();
+    set.add(start);
+
+    let arr = [];
+
+    while (queue.length) {
+        let curr = queue.shift();
+      arr.push(curr);
+
+      adjList[curr].forEach((el) => {
+          if (!set.has(el)) {
+              set.add(el);
+                queue.push(el);
+          }
+      })
+    }
+    return arr;
+  }
 
 console.log("First Test:")
-printBreadthFirst(3); // Prints 1 through 6 in Breadth-first order, starting with 3
+console.log(printBreadthFirst(3)); // Prints 1 through 6 in Breadth-first order, starting with 3
                     // One possible solution:  [ 3, 2, 4, 1, 5, 6 ]
 console.log("Second Test:")
-printBreadthFirst(6); // Prints 1 through 6 in Breadth-first order, starting with 6
+console.log(printBreadthFirst(6)); // Prints 1 through 6 in Breadth-first order, starting with 6
                     // One possible solution:  [ 6, 4, 3, 5, 2, 1 ]
 console.log("Third Test:")
-printBreadthFirst(4); // Prints 1 through 6 in Breadth-first order, starting with 4
+console.log(printBreadthFirst(4)); // Prints 1 through 6 in Breadth-first order, starting with 4
                         // One possible solution:  [ 4, 3, 5, 6, 2, 1 ]
