959d74b99c0932a749d6cb768921fff697a568ae
nate <nscott1010@gmail.com>
2022-06-16 12:24:42 -0500

    dft

--- week-6/d4-graphs/practices/01-graph-repl-problems/homework/02-simple-depth-first-traversal.js
+++ week-6/d4-graphs/practices/01-graph-repl-problems/homework/02-simple-depth-first-traversal.js
@@ -8,15 +8,30 @@ const adjList = {
 }
 
 function printDepthFirst(start) {
-    // your code here
+    let stack = [start];
+    let set = new Set();
+    set.add(start);
+
+    while (stack.length) {
+        let curr = stack.pop();
+        console.log(curr);
+
+        adjList[curr].forEach((el) => {
+            if (!set.has(el)) {
+                set.add(el);
+                stack.push(el);
+            }
+        })
+    }
 }
+
 
 console.log("First Test:")
 printDepthFirst(3); // Prints 1 through 6 in Breadth-first order, starting with 3
-                    // One possible solution:  3, 4, 6, 5, 1, 2
+// One possible solution:  3, 4, 6, 5, 1, 2
 console.log("Second Test:")
 printDepthFirst(6); // Prints 1 through 6 in Breadth-first order, starting with 6
-                    // One possible solution:  6, 4, 5, 2, 1, 3
+// One possible solution:  6, 4, 5, 2, 1, 3
 console.log("Third Test:")
 printDepthFirst(4); // Prints 1 through 6 in Breadth-first order, starting with 4
                     // One possible solution:  4, 6, 5, 2, 1, 3
