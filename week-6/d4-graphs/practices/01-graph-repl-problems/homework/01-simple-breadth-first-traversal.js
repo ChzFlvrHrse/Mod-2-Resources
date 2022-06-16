1d97ebce4b647030a57065be52669d93bbfe9662
nate <nscott1010@gmail.com>
2022-06-16 12:23:26 -0500

    bft

--- week-6/d4-graphs/practices/01-graph-repl-problems/homework/01-simple-breadth-first-traversal.js
+++ week-6/d4-graphs/practices/01-graph-repl-problems/homework/01-simple-breadth-first-traversal.js
@@ -8,15 +8,29 @@ const adjList = {
 }
 
 function printBreadthFirst(start) {
-    // your code here
-}
+    let queue = [start];
+    let set = new Set();
+    set.add(start);
+
+    while (queue.length) {
+        let curr = queue.shift();
+      console.log(curr);
+
+      adjList[curr].forEach((el) => {
+          if (!set.has(el)) {
+              set.add(el);
+                queue.push(el);
+          }
+      })
+    }
+  }
 
 console.log("First Test:")
 printBreadthFirst(3); // Prints 1 through 6 in Breadth-first order, starting with 3
-                    // One possible solution:  3, 2, 4, 1, 5, 6
+// One possible solution:  3, 2, 4, 1, 5, 6
 console.log("Second Test:")
 printBreadthFirst(6); // Prints 1 through 6 in Breadth-first order, starting with 6
-                    // One possible solution:  6, 4, 3, 5, 2, 1
+// One possible solution:  6, 4, 3, 5, 2, 1
 console.log("Third Test:")
 printBreadthFirst(4); // Prints 1 through 6 in Breadth-first order, starting with 4
                     // One possible solution:  4, 3, 5, 6, 2, 1
